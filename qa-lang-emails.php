cb74a5d26a8540997d8e1845b47c7e98f7f2b0e9
tomheng <zhm20070928@gmail.com>
2012-02-08 22:08:55 +0800

    qa-lang-email.php translate

--- qa-lang-emails.php
+++ qa-lang-emails.php
@@ -10,6 +10,12 @@
 	Version: See define()s at top of qa-include/qa-base.php
 	Description: Language phrases for email notifications
 
+	--------------Translate to chiness-------------
+	Author: tomheng 
+	Created: 2012-02-08
+	Blog: http://blog.webfuns.net
+	Git: git@github.com:tomheng/Question2Answer_zh.git
+	------------------------------------------------
 
 	This program is free software; you can redistribute it and/or
 	modify it under the terms of the GNU General Public License
@@ -25,55 +31,55 @@
 */
 
 	return array(
-		'a_commented_body' => "Your answer on ^site_title has a new comment by ^c_handle:\n\n^open^c_content^close\n\nYour answer was:\n\n^open^c_context^close\n\nYou may respond by adding your own comment:\n\n^url\n\nThank you,\n\n^site_title",
-		'a_commented_subject' => 'Your ^site_title answer has a new comment',
+		'a_commented_body' => "你在^site_title 回复 ^c_handle 评论了:\n\n^open^c_content^close\n\n你的回复:\n\n^open^c_context^close\n\n点击此链接可以回复他:\n\n^url\n\nThank you,\n\n^site_title",
+		'a_commented_subject' => '你在^site_title的回复有了新评论',
 
-		'a_followed_body' => "Your answer on ^site_title has a new related question by ^q_handle:\n\n^open^q_title^close\n\nYour answer was:\n\n^open^a_content^close\n\nClick below to answer the new question:\n\n^url\n\nThank you,\n\n^site_title",
-		'a_followed_subject' => 'Your ^site_title answer has a related question',
+		'a_followed_body' => "你在^site_title的回复， ^q_handle 提出了一个相关问题:\n\n^open^q_title^close\n\n 你的回复:\n\n^open^a_content^close\n\n 点击下面的链接回复此问题:\n\n^url\n\n 谢谢,\n\n^site_title",
+		'a_followed_subject' => '你在 ^site_title 的回复有了相似提问',
 
-		'a_selected_body' => "Congratulations! Your answer on ^site_title has been selected as the best by ^s_handle:\n\n^open^a_content^close\n\nThe question was:\n\n^open^q_title^close\n\nClick below to see your answer:\n\n^url\n\nThank you,\n\n^site_title",
-		'a_selected_subject' => 'Your ^site_title answer has been selected!',
+		'a_selected_body' => "祝贺! 你在^site_title的回复被 ^s_handle选为最佳答案:\n\n^open^a_content^close\n\n 问题:\n\n^open^q_title^close\n\n点击链接查看问题:\n\n^url\n\n谢谢,\n\n^site_title",
+		'a_selected_subject' => '你在^site_title的回复被选为最佳答案!',
 
-		'c_commented_body' => "A new comment by ^c_handle has been added after your comment on ^site_title:\n\n^open^c_content^close\n\nThe discussion is following:\n\n^open^c_context^close\n\nYou may respond by adding another comment:\n\n^url\n\nThank you,\n\n^site_title",
-		'c_commented_subject' => 'Your ^site_title comment has been added to',
+		'c_commented_body' => " ^c_handle在 ^site_title回复了你的评论:\n\n^open^c_content^close\n\n 讨论内容:\n\n^open^c_context^close\n\n 你可以回复他:\n\n^url\n\n 谢谢,\n\n^site_title",
+		'c_commented_subject' => '在^site_title有人回复了你的评论',
 
-		'confirm_body' => "Please click below to confirm your email address for ^site_title.\n\n^url\n\nThank you,\n^site_title",
-		'confirm_subject' => '^site_title - Email Address Confirmation',
+		'confirm_body' => "点击下面的链接确认你在 ^site_title的注册邮箱.\n\n^url\n\n谢谢,\n^site_title",
+		'confirm_subject' => '^site_title - 请确认您的邮箱',
 
-		'feedback_body' => "Comments:\n^message\n\nName:\n^name\n\nEmail:\n^email\n\nPrevious page:\n^previous\n\nUser:\n^url\n\nIP address:\n^ip\n\nBrowser:\n^browser",
-		'feedback_subject' => '^ feedback',
+		'feedback_body' => "反馈内容:\n^message\n\n 姓名:\n^name\n\n 邮箱:\n^email\n\n 链接:\n^previous\n\n 用户:\n^url\n\nIP地址:\n^ip\n\n浏览器:\n^browser",
+		'feedback_subject' => '^ 反馈',
 
-		'flagged_body' => "A post by ^p_handle has received ^flags:\n\n^open^p_context^close\n\nClick below to see the post:\n\n^url\n\nThank you,\n\n^site_title",
-		'flagged_subject' => '^site_title has a flagged post',
+		'flagged_body' => "^p_handle 被标记为^flags:\n\n^open^p_context^close\n\n点击查看:\n\n^url\n\n谢谢,\n\n^site_title",
+		'flagged_subject' => '^site_title有被标记的提问',
 
-		'moderate_body' => "A post by ^p_handle requires your approval:\n\n^open^p_context^close\n\nClick below to approve or reject the post:\n\n^url\n\nThank you,\n\n^site_title",
-		'moderate_subject' => '^site_title moderation',
+		'moderate_body' => " 批准^p_handle的提问:\n\n^open^p_context^close\n\n 点击批准或驳回:\n\n^url\n\n谢谢,\n\n^site_title",
+		'moderate_subject' => '^site_title 有需要审核的内容',
 
-		'new_password_body' => "Your new password for ^site_title is below.\n\nPassword: ^password\n\nIt is recommended to change this password immediately after logging in.\n\nThank you,\n^site_title\n^url",
-		'new_password_subject' => '^site_title - Your New Password',
+		'new_password_body' => "你在 ^site_title的新密码.\n\n密码: ^password\n\n建议登录后立即修改密码\n\n谢谢,\n^site_title\n^url",
+		'new_password_subject' => '^site_title - 新密码',
 
-		'private_message_body' => "You have been sent a private message by ^f_handle on ^site_title:\n\n^open^message^close\n\n^moreThank you,\n\n^site_title\n\n\nTo block private messages, visit your account page:\n^a_url",
-		'private_message_info' => "More information about ^f_handle:\n\n^url\n\n",
-		'private_message_reply' => "Click below to reply to ^f_handle by private message:\n\n^url\n\n",
-		'private_message_subject' => 'Message from ^f_handle on ^site_title',
+		'private_message_body' => "你在^site_title 上收到^f_handle的私信:\n\n^open^message^close\n\n^more 谢谢,\n\n^site_title\n\n\n可以在账户设置页面关闭私信功能:\n^a_url",
+		'private_message_info' => "关于^f_handle:\n\n^url\n\n",
+		'private_message_reply' => "点击给^f_handle发送私信:\n\n^url\n\n",
+		'private_message_subject' => '^f_handle在^site_title给你发送了消息',
 
-		'q_answered_body' => "Your question on ^site_title has been answered by ^a_handle:\n\n^open^a_content^close\n\nYour question was:\n\n^open^q_title^close\n\nIf you like this answer, you may select it as the best:\n\n^url\n\nThank you,\n\n^site_title",
-		'q_answered_subject' => 'Your ^site_title question was answered',
+		'q_answered_body' => "^a_handle回复了你在^site_title的问题:\n\n^open^a_content^close\n\n你的问题:\n\n^open^q_title^close\n\n点击链接，将这个回复作为最佳答案:\n\n^url\n\n谢谢,\n\n^site_title",
+		'q_answered_subject' => '有人回复了你在^site_title的问题',
 
-		'q_commented_body' => "Your question on ^site_title has a new comment by ^c_handle:\n\n^open^c_content^close\n\nYour question was:\n\n^open^c_context^close\n\nYou may respond by adding your own comment:\n\n^url\n\nThank you,\n\n^site_title",
-		'q_commented_subject' => 'Your ^site_title question has a new comment',
+		'q_commented_body' => " ^c_handle 评论了你在^site_title的问题:\n\n^open^c_content^close\n\n你的问题:\n\n^open^c_context^close\n\n点击链接回复他:\n\n^url\n\n谢谢,\n\n^site_title",
+		'q_commented_subject' => '你的问题有了新评论， ^site_title',
 
-		'q_posted_body' => "A new question has been asked by ^q_handle:\n\n^open^q_title\n\n^q_content^close\n\nClick below to see the question:\n\n^url\n\nThank you,\n\n^site_title",
-		'q_posted_subject' => '^site_title has a new question',
+		'q_posted_body' => "^q_handle提了一个问题:\n\n^open^q_title\n\n^q_content^close\n\n点击链接查看新问题:\n\n^url\n\n谢谢,\n\n^site_title",
+		'q_posted_subject' => '^site_title上有了新问题',
 
-		'reset_body' => "Please click below to reset your password for ^site_title.\n\n^url\n\nAlternatively, enter the code below into the field provided.\n\nCode: ^code\n\nIf you did not ask to reset your password, please ignore this message.\n\nThank you,\n^site_title",
-		'reset_subject' => '^site_title - Reset Forgotten Password',
+		'reset_body' => "点击下面的链接重置密码。\n\n^url\n\n或者，将下面的验证码出入重置密码表单。\n\n验证码: ^code\n\n如果你没有申请重置密码，请忽略本邮件。\n\n 谢谢,\n^site_title",
+		'reset_subject' => '^site_title - 找回密码',
 
 		'to_handle_prefix' => "^,\n\n",
 
-		'welcome_body' => "Thank you for registering for ^site_title.\n\n^custom^confirmYour login details are as follows:\n\nEmail: ^email\nPassword: ^password\n\nPlease keep this information safe for future reference.\n\nThank you,\n\n^site_title\n^url",
-		'welcome_confirm' => "Please click below to confirm your email address.\n\n^url\n\n",
-		'welcome_subject' => 'Welcome to ^site_title!',
+		'welcome_body' => "感谢注册^site_title.\n\n^custom^confirm 你的登录信息:\n\n 邮箱: ^email\n 密码: ^password\n\n请妥善保管此邮件，以备后查。\n\n谢谢,\n\n^site_title\n^url",
+		'welcome_confirm' => "点击下面的链接确认邮件地址.\n\n^url\n\n",
+		'welcome_subject' => '欢迎加入^site_title!',
 	);
 	
 
