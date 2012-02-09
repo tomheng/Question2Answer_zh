1d99a1c50e8804414963d9fa69b3767e19dead4b
tomheng <zhm20070928@gmail.com>
2012-02-09 23:01:49 +0800

    profile

--- qa-lang-profile.php
+++ qa-lang-profile.php
@@ -10,6 +10,13 @@
 	Version: See define()s at top of qa-include/qa-base.php
 	Description: Language phrases for user profile page
 
+	
+	--------------Translate to chiness-------------
+	Author: tomheng 
+	Created: 2012-02-09
+	Blog: http://blog.webfuns.net
+	Git: git@github.com:tomheng/Question2Answer_zh.git
+	------------------------------------------------
 
 	This program is free software; you can redistribute it and/or
 	modify it under the terms of the GNU General Public License
@@ -25,51 +32,51 @@
 */
 
 	return array(
-		'1_chosen_as_best' => ' (1 chosen as best)',
-		'1_down_vote' => '1 down vote',
-		'1_up_vote' => '1 up vote',
-		'1_with_best_chosen' => ' (1 with best answer chosen)',
-		'activity_by_x' => 'Activity by ^',
-		'answers' => 'Answers:',
-		'bonus_points' => 'Bonus points:',
-		'comments' => 'Comments:',
-		'extra_privileges' => 'Extra privileges:',
-		'gave_out' => 'Gave out:',
-		'my_account_title' => 'My account details',
-		'no_posts_by_x' => 'No posts by ^',
-		'permit_anon_view_ips' => 'Viewing IPs of anonymous posts',
-		'permit_close_q' => 'Closing any question',
-		'permit_delete_hidden' => 'Deleting hidden posts',
-		'permit_edit_a' => 'Editing any answer',
-		'permit_edit_c' => 'Editing any comment',
-		'permit_edit_q' => 'Editing any question',
+		'1_chosen_as_best' => ' (一个最佳答案)',
+		'1_down_vote' => '1人反对',
+		'1_up_vote' => '1人赞成',
+		'1_with_best_chosen' => ' (一个最佳答案)',
+		'activity_by_x' => '^最后回复',
+		'answers' => '回复:',
+		'bonus_points' => '奖励积分:',
+		'comments' => '评论:',
+		'extra_privileges' => '附加权限:',
+		'gave_out' => '给出:',
+		'my_account_title' => '我的账户',
+		'no_posts_by_x' => '^还没有提问',
+		'permit_anon_view_ips' => '匿名用户提问显示IP',
+		'permit_close_q' => '关闭任何问题',
+		'permit_delete_hidden' => '删除隐藏内容',
+		'permit_edit_a' => '编辑回复',
+		'permit_edit_c' => '编辑评论',
+		'permit_edit_q' => '编辑问题',
 		'permit_flag' => 'Flagging posts',
-		'permit_hide_show' => 'Hiding or showing any post',
-		'permit_moderate' => 'Approving or rejecting posts',
-		'permit_post_a' => 'Answering questions',
-		'permit_post_c' => 'Adding comments',
-		'permit_post_q' => 'Asking questions',
-		'permit_recat' => 'Recategorizing any question',
-		'permit_retag' => 'Retagging any question',
+		'permit_hide_show' => '隐藏、显示内容',
+		'permit_moderate' => '批准、驳回内容',
+		'permit_post_a' => '回复问题',
+		'permit_post_c' => '添加评论',
+		'permit_post_q' => '提问',
+		'permit_recat' => '允许更改分类',
+		'permit_retag' => '允许更改标签',
 		'permit_select_a' => 'Selecting answer for any question',
-		'permit_view_q_page' => 'Viewing question pages',
-		'permit_vote_a' => 'Voting on answers',
-		'permit_vote_down' => 'Voting posts down',
-		'permit_vote_q' => 'Voting on questions',
-		'questions' => 'Questions:',
-		'ranked_x' => ' (ranked #^)',
-		'received' => 'Received:',
-		'recent_activity_by_x' => 'Recent activity by ^',
-		'score' => 'Score:',
-		'send_private_message' => ' - ^1send private message^2',
-		'set_bonus_button' => 'Update bonus',
-		'title' => 'Title:',
-		'user_x' => 'User ^',
-		'voted_on' => 'Voted on:',
-		'x_chosen_as_best' => ' (^ chosen as best)',
-		'x_down_votes' => '^ down votes',
-		'x_up_votes' => '^ up votes',
-		'x_with_best_chosen' => ' (^ with best answer chosen)',
+		'permit_view_q_page' => '查看问题页面',
+		'permit_vote_a' => '对回复投票',
+		'permit_vote_down' => '反对',
+		'permit_vote_q' => '对问题投票',
+		'questions' => '问题:',
+		'ranked_x' => ' (积分 #^)',
+		'received' => '收到:',
+		'recent_activity_by_x' => '^的最近活动',
+		'score' => '积分:',
+		'send_private_message' => ' - ^1私信^2',
+		'set_bonus_button' => '更新积分',
+		'title' => '头衔:',
+		'user_x' => '用户 ^',
+		'voted_on' => '投票:',
+		'x_chosen_as_best' => ' (^选为最佳)',
+		'x_down_votes' => '^人反对',
+		'x_up_votes' => '^ 人赞成',
+		'x_with_best_chosen' => ' (^ 个最佳答案)',
 	);
 	
 
