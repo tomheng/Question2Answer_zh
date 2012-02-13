c0326725d3918e75e39da40bbba157d834bee45b
tomheng <zhm20070928@gmail.com>
2012-02-13 17:12:40 +0800

    fixed date translate

---
+++ qa-lang-misc.php
@@ -0,0 +1,101 @@
+<?php
+	
+/*
+	Question2Answer (c) Gideon Greenspan
+
+	http://www.question2answer.org/
+
+	
+	File: qa-include/qa-lang-misc.php
+	Version: See define()s at top of qa-include/qa-base.php
+	Description: Miscellaneous language phrases
+
+	--------------Translate to chiness-------------
+	Author: tomheng 
+	Created: 2012-02-08
+	Blog: http://blog.webfuns.net
+	Git: git@github.com:tomheng/Question2Answer_zh.git
+	------------------------------------------------	
+	
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+
+	return array(
+		'block_ip_button' => '禁止的IP地址 ',
+		'browse_categories' => '浏览目录',
+		'captcha_confirm_fix' => '不想看到验证码, 请 ^5确认邮件地址^6.',
+		'captcha_error' => '请输入正确验证',
+		'captcha_label' => '请输入验证码:',
+		'captcha_login_fix' => ' ^1登录^2 或 ^3注册^4后不会被要求输入验证码。',
+		'feed_a_edited_prefix' => '最后修改于: ',
+		'feed_a_prefix' => '回复于: ',
+		'feed_a_reshown_prefix' => '显示回复: ',
+		'feed_a_selected_prefix' => '最佳答案: ',
+		'feed_c_edited_prefix' => '评论修改于: ',
+		'feed_c_moved_prefix' => '移动评论到: ',
+		'feed_c_prefix' => '评论: ',
+		'feed_c_reshown_prefix' => '显示评论: ',
+		'feed_closed_prefix' => '关闭: ',
+		'feed_edited_prefix' => '编辑: ',
+		'feed_hidden_prefix' => '隐藏: ',
+		'feed_not_found' => '没有Feed',
+		'feed_recategorized_prefix' => '设置分类: ',
+		'feed_reopened_prefix' => '重新打开: ',
+		'feed_reshown_prefix' => '显示: ',
+		'feed_retagged_prefix' => '添加标签: ',
+		'feedback_email' => '邮箱: (可选)',
+		'feedback_empty' => '请使用该字段来发表意见或建议',
+		'feedback_message' => '对^的建议或意见:',
+		'feedback_name' => '名字: (可选)',
+		'feedback_sent' => '下面的信息已发送 - 谢谢。',
+		'feedback_title' => '反馈',
+		'hide_all_ip_button' => '隐藏此IP的所有内容',
+		'host_name' => '主机名字:',
+		'matches_blocked_ips' => '被屏蔽的IP地址:',
+		'message_empty' => '请输入要发送给此用户的内容',
+		'message_explanation' => 'This will be sent as a notification from ^. Your email address will not be revealed unless you include it in the message.',
+		'message_for_x' => 'Your message for ^:',
+		'message_limit' => '遇到错误，请稍后重试。',
+		'message_must_login' => '请^1登录^2 或 ^3注册^4后发送年私信。',
+		'message_received_x_ago' => '^前收到:',
+		'message_recent_history' => 'Recent correspondence with ^',
+		'message_sent_x_ago' => '^发送:',
+		'message_sent' => '私信已发送',
+		'my_favorites_title' => '我的收藏',
+		'nav_all_my_updates' => 'All my updates',
+		'nav_my_content' => '我的内容',
+		'nav_my_details' => 'My details',
+		'nav_my_favorites' => '我的收藏',
+		'no_activity_from_x' => '^没有任何动态',
+		'no_favorite_categories' => '没有收藏的分类',
+		'no_favorite_qs' => '没有收藏的问题',
+		'no_favorite_tags' => '没有收藏标签',
+		'no_favorite_users' => '没有喜欢的用户',
+		'no_recent_updates' => '没有更新',
+		'no_updates_content' => 'No recent updates for my content',
+		'no_updates_favorites' => '收藏中没有新内容',
+		'private_message_title' => '发送私信',
+		'recent_activity_from_x' => 'Recent activity from ^',
+		'recent_updates_content' => 'Recent updates for my content',
+		'recent_updates_favorites' => 'Recent updates for my favorites',
+		'recent_updates_title' => 'Recent updates for me',
+		'site_in_maintenance' => '网站正在维护 - 请稍后再来。',
+		'suggest_favorites_add' => '请点击页面顶部的 ^添加到收藏。',
+		'suggest_update_favorites' => '^1收藏^2以获得随后的更新',
+		'unblock_ip_button' => 'IP地址白名单',
+	);
+	
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
