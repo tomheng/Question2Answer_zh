9cd3dbb0af8c2fae3223bf14c15f80f9e1bf9505
tomheng <zhm20070928@gmail.com>
2012-02-21 14:26:36 +0800

    test

---
+++ qa-lang-profile.php
@@ -0,0 +1,85 @@
+<?php
+	
+/*
+	Question2Answer (c) Gideon Greenspan
+
+	http://www.question2answer.org/
+
+	
+	File: qa-include/qa-lang-profile.php
+	Version: See define()s at top of qa-include/qa-base.php
+	Description: Language phrases for user profile page
+
+	
+	--------------Translate to chiness-------------
+	Author: tomheng 
+	Created: 2012-02-09
+	Blog: http://blog.webfuns.net
+	Git: git@github.com:tomheng/Question2Answer_zh.git
+	------------------------------------------------
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+
+	return array(
+		'1_chosen_as_best' => ' (一个最佳答案)',
+		'1_down_vote' => '1人反对',
+		'1_up_vote' => '1人赞成',
+		'1_with_best_chosen' => ' (一个最佳答案)',
+		'activity_by_x' => '^最后回复',
+		'answers' => '回复:',
+		'bonus_points' => '奖励积分:',
+		'comments' => '评论:',
+		'extra_privileges' => '附加权限:',
+		'gave_out' => '给出:',
+		'my_account_title' => '我的账户',
+		'no_posts_by_x' => '^还没有提问',
+		'permit_anon_view_ips' => '匿名用户提问显示IP',
+		'permit_close_q' => '关闭任何问题',
+		'permit_delete_hidden' => '删除隐藏内容',
+		'permit_edit_a' => '编辑回复',
+		'permit_edit_c' => '编辑评论',
+		'permit_edit_q' => '编辑问题',
+		'permit_flag' => '举报成功的内容',
+		'permit_hide_show' => '隐藏、显示内容',
+		'permit_moderate' => '批准、驳回内容',
+		'permit_post_a' => '回复问题',
+		'permit_post_c' => '添加评论',
+		'permit_post_q' => '提问',
+		'permit_recat' => '允许更改分类',
+		'permit_retag' => '允许更改标签',
+		'permit_select_a' => 'Selecting answer for any question',
+		'permit_view_q_page' => '查看问题页面',
+		'permit_vote_a' => '对回复投票',
+		'permit_vote_down' => '反对',
+		'permit_vote_q' => '对问题投票',
+		'questions' => '问题:',
+		'ranked_x' => ' (积分 #^)',
+		'received' => '收到:',
+		'recent_activity_by_x' => '^的最近活动',
+		'score' => '积分:',
+		'send_private_message' => ' - ^1私信^2',
+		'set_bonus_button' => '更新积分',
+		'title' => '头衔:',
+		'user_x' => '用户 ^',
+		'voted_on' => '投票:',
+		'x_chosen_as_best' => ' (^选为最佳)',
+		'x_down_votes' => '^人反对',
+		'x_up_votes' => '^ 人赞成',
+		'x_with_best_chosen' => ' (^ 个最佳答案)',
+	);
+	
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
---
+++ qa-lang-question.php
@@ -0,0 +1,172 @@
+<?php
+	
+/*
+	Question2Answer (c) Gideon Greenspan
+
+	http://www.question2answer.org/
+
+	
+	File: qa-include/qa-lang-question.php
+	Version: See define()s at top of qa-include/qa-base.php
+	Description: Language phrases for question page
+
+	
+	--------------Translate to chiness-------------
+	Author: tomheng 
+	Created: 2012-02-09
+	Blog: http://blog.webfuns.net
+	Git: git@github.com:tomheng/Question2Answer_zh.git
+	------------------------------------------------
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+
+	return array(
+		'1_answer_title' => '1条回复',
+		'a_convert_to_c_on' => '将这条回复转换成评论到:',
+		'a_convert_to_c' => '将此条回复转换为评论',
+		'a_convert_warn_cs' => '警告: 此操作不能撤销，并且会影响到此下所有评论。',
+		'a_convert_warn' => '警告: 此操作不能撤销。',
+		'a_notify_email' => '如果我的回复被选中或被评论，请通过一下邮箱通知我:',
+		'a_notify_label' => '如果我的回复被选中或者被评论，请邮件通知我',
+		'a_notify_x_label' => '如果我的回复被选中或有人评论，请通过(^)邮件通知我。 ',
+		'a_waiting_your_approval' => '这个回复须要批准',
+		'a_your_waiting_approval' => '你的回复稍后会有人查看并批准',
+		'add_answer_button' => '添加回复',
+		'add_comment_button' => '添加评论',
+		'add_q_favorites' => '收藏此问题',
+		'answer_button' => '回复',
+		'answer_limit' => '太多回复，请一小时后重试。',
+		'answer_must_confirm' => '请^5验证邮箱^6后回答问题。',
+		'answer_must_login' => '请^1登录^2或^3注册^4后回复此问题。',
+		'answer_q_popup' => '回复此问题',
+		'approve_button' => '批准',
+		'ask_button' => '提问',
+		'ask_follow_from_a' => '你的问题将与一下回复关联:',
+		'ask_follow_title' => '问一个关联问题',
+		'ask_limit' => '太多提问 - 请一小时后重试',
+		'ask_must_confirm' => '请^5验证邮箱^6后提问。',
+		'ask_must_login' => '请^1登录^2或^3注册^4后提问。',
+		'ask_same_q' => '提问前，请先确认你的问题是否有人提问过:',
+		'ask_title' => '提一个问题',
+		'c_notify_email' => '有用户在我之后发表评论时，请邮件通知我:',
+                'c_notify_label' => '有用户发表评论时，请邮件通知我',
+                'c_notify_x_label' => '有用户发表评论时，请通过邮件 (^) 通知我',
+		'c_waiting_your_approval' => '这条评论等待批准',
+		'c_your_waiting_approval' => '你的评论稍后会被审核。',
+		'category_js_note' => '请启用你浏览器中Javascript支持，来选择分类目录。',
+		'category_required' => '请选择一个分类目录',
+		'claim_button' => '这是我提问的',
+		'clear_flags_button' => '驳回举报',
+		'clear_flags_popup' => '清理所有用户举报',
+		'close_button' => '关闭',
+		'close_duplicate_error' => '没有找到重复问题 - 请尝试输入问题的数字编号（问题页面URL的数字部分），例如：123',
+		'close_duplicate' => '和另一个问题重复了',
+		'close_form_button' => '关闭问题',
+		'close_form_title' => '关闭这个问题',
+		'close_original_note' => '可以直接输入问题的数字编号（问题页面URL的数字部分），例如：123',
+		'close_original_title' => '原问题的链接:',
+		'close_q_popup' => '关闭问题，不接受任何回复',
+		'close_reason_title' => '关闭问题的原因:',
+		'closed_as_duplicate' => '关闭重复问题:',
+		'closed_with_note' => '关闭说明:',
+		'comment_a_popup' => '为回复添加评论',
+		'comment_button' => '评论',
+		'comment_limit' => '评论太快，请一个小时后重试。',
+		'comment_must_confirm' => '请 ^5确认邮箱^6后发表评论',
+		'comment_must_login' => '请^1登录^2或^3注册^4后发表评论。',
+		'comment_on_a' => '回复于: ',
+		'comment_on_q' => '提问于: ',
+		'comment_q_popup' => '评论该问题',
+		'delete_a_popup' => '永久删除此回复',
+		'delete_button' => '删除',
+		'delete_c_popup' => '永久删除此评论',
+		'delete_q_popup' => '永久删除此问题',
+		'duplicate_content' => '重复内容提交',
+		'edit_a_popup' => '编辑此回复',
+		'edit_a_title' => '编辑回复',
+		'edit_button' => '编辑',
+		'edit_c_popup' => '编辑此评论',
+		'edit_c_title' => '编辑评论',
+		'edit_must_confirm' => '请^5确认邮箱^6后编辑',
+		'edit_must_login' => '请^1登录^2或^3注册^4后编辑',
+		'edit_q_popup' => '编辑此问题',
+		'edit_q_title' => '编辑问题',
+		'example_tags' => '示例标签: ',
+		'flag_a_popup' => '举报此问题为垃圾或者不适合内容',
+		'flag_button' => '举报',
+		'flag_c_popup' => '举报此评论为垃圾或不适合内容',
+		'flag_hide_button' => '举报并隐藏',
+		'flag_limit' => '举报内容太多-请一小时后重试',
+		'flag_must_confirm' => '请^5验证邮箱^6后举报.',
+		'flag_must_login' => '请^1登录^2 或 ^3注册^4后举报',
+		'flag_not_allowed' => '无法完成举报',
+		'flag_q_popup' => '举报此问题为垃圾信息或不适合内容',
+		'follow_a_popup' => '问一个与此相关的问题',
+		'follow_button' => '问一个相关问题',
+		'follows_a' => 'related to an answer for:',
+		'follows_q' => 'about the question:',
+		'hide_a_popup' => '隐藏这个问题',
+		'hide_button' => '隐藏',
+		'hide_c_popup' => '隐藏这个评论',
+		'hide_q_popup' => '隐藏这个问题',
+		'matching_tags' => '匹配标签: ',
+		'max_tags_x' => '标签不能超过^个',
+		'min_tags_x' => '至少设置^个标签',
+		'notify_email_note' => '隐私保护: 您的邮箱仅用于发送系统通知。',
+		'q_category_label' => '分类:',
+		'q_content_label' => '请详细描述你的问题:',
+		'q_hidden_author' => '此问题已被作者隐藏',
+		'q_hidden_flagged' => '问题因举报被隐藏',
+		'q_hidden_other' => '此问题已被隐藏',
+		'q_notify_email' => '如果问题有了回复或评论，请通过一下邮件通知我:',
+		'q_notify_label' => '如果问题有了回复或评论，请邮件通知我',
+		'q_notify_x_label' => '如果问题有了回复或评论，请通过邮件(^) 通知我',
+		'q_tags_comma_label' => '标签(使用,分割) :',
+		'q_tags_label' => '标签（单词之间用连字符连接）:',
+		'q_title_label' => '请用一句话描述你的问题:',
+		'q_waiting_approval' => '问题须要你的批准',
+		'q_waiting_your_approval' => '问题须要你的批准',
+		'q_your_waiting_approval' => '问题很快会被查看审核。',
+		'recat_button' => '更改分类',
+		'recat_popup' => '修改这个问题的分类',
+		'recat_q_title' => '更改问题分类',
+		'reject_button' => '反对',
+		'remove_q_favorites' => '取消收藏',
+		'reopen_button' => '重新打开',
+		'reply_button' => '回复',
+		'reply_c_popup' => '回复此评论',
+		'reshow_button' => 'reshow',
+		'retag_button' => '修改标签',
+		'retag_cat_popup' => '修改问题分类或标签',
+		'retag_popup' => '修改此问题的标签',
+		'retag_q_title' => '修改问题标签',
+		'select_popup' => '点击按钮选择最佳答案',
+		'select_text' => '最佳答案',
+		'show_1_comment' => '显示一个评论',
+		'show_1_previous_comment' => 'Show 1 previous comment',
+		'show_x_comments' => '显示^个评论',
+		'show_x_previous_comments' => 'Show ^ previous comments',
+		'unflag_button' => '取消举报',
+		'unflag_popup' => '取消对此的举报',
+		'unselect_popup' => '点击取消选择',
+		'x_answers_title' => '^个回复',
+		'your_answer_title' => '你的回复',
+		'your_comment_a' => '你的评论:',
+		'your_comment_q' => '你对此问题的评论:',
+	);
+	
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
